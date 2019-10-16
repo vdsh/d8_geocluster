4b3fcca0c127fccfe0c2a6153851458f24273573
vdsh <sroced@gmail.com>
2019-10-16 23:22:52 +0200

    Handling popup & titles in Leaflet (with leaflet_geojson)

--- js/geocluster.leaflet.bbox.js
+++ js/geocluster.leaflet.bbox.js
@@ -2,16 +2,11 @@
 
   // Replace the default options for our use case.
   Drupal.leafletBBox.geoJSONOptions = {
-
+    
     pointToLayer: function(featureData, latlng) {
       if (featureData.properties.geocluster_count > 1) {
         var number = featureData.properties.geocluster_count;
-          /*
-        if (number >= 1000) {
-          number = number.toString().substring(0, number.toString().length -3) + 'k';
-        }
-        */
-        //var icon = new L.NumberedDivIcon({number: number});
+
 
           var c = ' marker-cluster-';
           if (number < 10) {
@@ -30,21 +25,25 @@
           lMarker = new L.Marker(latlng, {icon:icon});
       }
       else {
-        lMarker = new L.Marker(latlng);
+        title = "";
+        if (featureData.properties.label) {
+          title = featureData.properties.label;
+        }
+        lMarker = new L.Marker(latlng, {title: title});
       }
       return lMarker;
     },
 
-    onEachFeature: function(featureData, layer) {
-      var popupText = featureData.properties.name;
-
+    onEachFeature: function(featureData, layer) { 
       if (featureData.properties.geocluster_count > 1) {
         layer.on('click', function(e) {
           Drupal.leafletBBox.geoJSONOptions.clickOnClustered(e, featureData, layer);
         });
       }
       else {
-        layer.bindPopup(popupText);
+        if (featureData.properties && featureData.properties.popup) {
+          layer.bindPopup(featureData.properties.popup);
+        }
       }
     },
 
